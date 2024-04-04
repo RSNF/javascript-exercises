6a605424e936699bcc6d5bcaf34fd0c43347c5c4
RSNF <ray1001novaes@gmail.com>
2024-04-04 18:02:32 -0300

    feat: js exercise 03 >:(

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str == "") return str;
+    return str.at(-1) + reverseString(str.slice(0, -1));
 };
 
 // Do not edit below this line
