bab97f56f48fa63cc48baa92a8671ff4a9c1af31
RSNF <ray1001novaes@gmail.com>
2024-04-05 18:04:22 -0300

    feat: js exercise 04 :'(
    no recursion...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < elements.length; j++) {
+            if (arr[i] === elements[j]) {
+                arr.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
