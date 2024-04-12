b3cd54ac79ad5f87f1e9d932a8c1cf7a6af0329b
RSNF <ray1001novaes@gmail.com>
2024-04-12 15:36:43 -0300

    feat: js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    let prev0 = 0;
+    let prev1 = 1;
+    let fib = 0;
+    for (let i = 0; i < num; i++) {
+
+        prev0 = prev1;
+
+        prev1 = fib;
+
+        fib = prev0 + prev1;
+    }
+
+    return fib;
 };
 
 // Do not edit below this line
