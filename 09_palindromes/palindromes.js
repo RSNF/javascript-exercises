48ab0407f764008c9e09c2911820c41fa2e30f57
RSNF <ray1001novaes@gmail.com>
2024-04-12 15:19:04 -0300

    feat: js exercise 09
    bad.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let clearString = str.split(",").join("").split("");
+    if (clearString.at(clearString.length - 1) == "." || 
+        clearString.at(clearString.length - 1) == "?" ||
+        clearString.at(clearString.length - 1) == "!") {
+        clearString.splice(clearString.length - 1, 1);
+    }
+    clearString = clearString.join("").split(" ").join("").toLowerCase();
 
+    if (clearString === clearString.split("").reverse().join("")) return true;
+
+    return false;
 };
+
+palindromes('racecar')
 
 // Do not edit below this line
 module.exports = palindromes;
