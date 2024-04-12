8f9c960d8951159a8f8dc54cb63028a62e82d172
RSNF <ray1001novaes@gmail.com>
2024-04-12 14:47:59 -0300

    feat: js exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acumulator, current) => acumulator + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((acumulator, current) => acumulator * current, 1);
 
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let element = 1;
+  for (let i = 0; i < power; i++) {
+    element = element * base;
+  }
+  return element;
 };
 
-const factorial = function() {
-	
+const factorial = function(fnum) {
+  if (fnum == 0) return 1;
+  return fnum * factorial(fnum - 1);
 };
 
 // Do not edit below this line
