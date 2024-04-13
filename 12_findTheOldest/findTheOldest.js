18de2e8f39ce64d69cdfbae8beab207eba3b255c
RSNF <ray1001novaes@gmail.com>
2024-04-12 21:49:30 -0300

    feat: js exercise 12
    bad.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((a, c) => {
 
+        let cond0 = !("yearOfDeath" in c) && (a.yearOfDeath - a.yearOfBirth) < (new Date().getFullYear() - c.yearOfBirth);
+        let cond1 = !("yearOfDeath" in a) && (new Date().getFullYear() - a.yearOfBirth) < (c.yearOfDeath - c.yearOfBirth);
+        let cond2 = (a.yearOfDeath - a.yearOfBirth) < (c.yearOfDeath - c.yearOfBirth);
+
+        if (cond0 || cond1 || cond2) {
+            a = c;
+        }
+
+        return a;
+    }, {name: "Foo", yearOfBirth: 1, yearOfDeath: 1});
 };
 
 // Do not edit below this line
